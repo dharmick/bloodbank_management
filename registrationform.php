a1a9fc38d47abf6dc7089cfec00fd18df5110ce0
dharmik <dharmik.joshi@somaiya.edu>
2018-10-04 12:58:35 +0530

    sendgrid added (not working)

--- registrationform.php
+++ registrationform.php
@@ -43,8 +43,51 @@ if(isset($_POST['submit']))
         $success=1;
         $_SESSION['success'] = $success;
       }
+
+
+    // You need to install the sendgrid client library so run:     
+    // composer require sendgrid/sendgrid
+    require './vendor/autoload.php';
+    
+    // contains a variable called: $API_KEY that is the API Key.
+    // You need this API_KEY created on the Sendgrid website.
+    $API_KEY="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";
+    
+    $FROM_EMAIL = 'dharmik.joshi@somaiya.edu';
+    // they dont like when it comes from @gmail, prefers business 
+    // emails
+    
+    $TO_EMAIL = 'parth.js@somaiya.edu';
+    // Try to be nice. Take a look at the anti spam laws. In most
+    // cases, you must have an unsubscribe. You also cannot be 
+    // misleading.
+    $subject = "YOUR_SUBJECT";
+    $from = new SendGrid\Email(null, $FROM_EMAIL);
+    $to = new SendGrid\Email(null, $TO_EMAIL);
+    $htmlContent = '';
+    // Create Sendgrid content
+    $content = new SendGrid\Content("text/html",$htmlContent);
+    // Create a mail object
+    $mail = new SendGrid\Mail($from, $subject, $to, $content);
+    
+    $sg = new \SendGrid($API_KEY);
+    $response = $sg->client->mail()->send()->post($mail);
+      
+    if ($response->statusCode() == 202) {
+     // Successfully sent
+     echo 'done';
+    } else {
+     echo 'false';
+    }
+
+
+
+
   }
 }
+
+
+
 ?>
 
 
