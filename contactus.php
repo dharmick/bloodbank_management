802d2ba932156f332cadf965e7fbc2788238a3d8
viralrambhia99 <viral.rambhia@somaiya.edu>
2018-10-17 11:38:37 +0530

    map position

--- contactus.php
+++ contactus.php
@@ -91,8 +91,9 @@
 	  </ol>
   </div>
   <!--breadcrumb ends-->
-
-  <section class="container">
+    <div class="container">
+    <div class="row">
+  <section class="container col-md-4">
   	<h3><strong>CONTACT Us</strong></h3>
   	<br><h4><b>Address</b></h4>
   	<p>Hope Drops Blood Bank,<br>3rd Floor, Somaiya Satsang Bhavan,<br>Western Express Highway,
@@ -110,11 +111,14 @@
  	<h4><b>License Number</b></h4>
  	<p>MH004755</p>
 
- 	<p>
-  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.761803069597!2d72.89753296437671!3d19.0742076569867!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c627923df00d%3A0x4c2b4fb28923f63d!2sSomaiya+Vidyavihar%2C+Group+of+Somaiya+Institutions%2C+Vidyanagar%2C+Vidya+Vihar+East%2C+Vidyavihar%2C+Mumbai%2C+Maharashtra+400077!5e0!3m2!1sen!2sin!4v1539718734649" width="600" height="450"  frameborder="0" style="border:0" allowfullscreen></iframe></p>
   </section>
-
-
+    <section class="col-md-8" style="
+    padding: 35px 15px 15px 15px;">
+         	<p>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.761803069597!2d72.89753296437671!3d19.0742076569867!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c627923df00d%3A0x4c2b4fb28923f63d!2sSomaiya+Vidyavihar%2C+Group+of+Somaiya+Institutions%2C+Vidyanagar%2C+Vidya+Vihar+East%2C+Vidyavihar%2C+Mumbai%2C+Maharashtra+400077!5e0!3m2!1sen!2sin!4v1539718734649" width="600" height="450"  frameborder="0" style="border:0" allowfullscreen></iframe></p>
+    </section>
+    </div>
+        </div>
  <?php include_once("footer.php") ?>
 
    <script type="text/javascript">
