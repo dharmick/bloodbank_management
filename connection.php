74719fb76c8f754e12e8e885f0d5626eb2d61605
dharmik <dharmik.joshi@somaiya.edu>
2018-10-31 22:51:09 +0530

    db4free added

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$server="localhost";
-$username="root";
-$password="";
-$db="bloodbank";
+$server="db4free.net";
+$username="hopedrops";
+$password="hopedrops";
+$db="hopedrops";
 $conn = mysqli_connect($server,$username,$password,$db);
 if(!$conn){
     die("Connection failed".mysqli_connect_error());
